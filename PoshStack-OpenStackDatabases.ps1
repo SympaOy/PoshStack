cfa327a461b22d150f816252e8616236ac2696a8
Don Schenck <don.schenck@gmail.com>
2015-05-20 14:04:40 -0400

    Issue #155 implemented

--- PoshStack-OpenStackDatabases.ps1
+++ PoshStack-OpenStackDatabases.ps1
@@ -343,6 +343,52 @@ function Get-OpenStackDatabaseUser {
     }
 }
 
+# Issue 155 Implement Remove-OpenStackDatabaseInstance
+function Remove-OpenStackDatabaseInstance {
+    Param(
+        [Parameter (Mandatory=$True)] [string] $Account = $(throw "Please specify required Cloud Account with -Account parameter"),
+        [Parameter (Mandatory=$True)] [string] $InstanceId = $(throw "Please specify required Instance ID with -InstanceId parameter"),
+        [Parameter (Mandatory=$False)][string] $RegionOverride
+        )
+
+    Get-OpenStackAccount -Account $Account
+    
+    if ($RegionOverride){
+        $Global:RegionOverride = $RegionOverride
+    }
+
+    # Use Region code associated with Account, or was an override provided?
+    if ($RegionOverride) {
+        $Region = $Global:RegionOverride
+    } else {
+        $Region = $Credentials.Region
+    }
+
+
+    $ComputeDatabasesProvider = Get-OpenStackDatabasesProvider -Account $Account -RegionOverride $Region
+
+    try {
+
+        # DEBUGGING       
+        Write-Debug -Message "Remove-OpenStackDatabaseInstance"
+        Write-Debug -Message "Account.......: $Account" 
+        Write-Debug -Message "InstanceId....: $InstanceId"
+        Write-Debug -Message "Region........: $Region"
+
+
+
+        $dbiid = New-Object([net.openstack.Providers.Rackspace.Objects.Databases.DatabaseInstanceId]) $InstanceId
+        $CancellationToken = New-Object ([System.Threading.CancellationToken]::None)
+        $AsyncCompletionOption = New-Object ([net.openstack.Core.AsyncCompletionOption])
+
+        $ComputeDatabasesProvider.RemoveDatabaseInstanceAsync($dbiid, $AsyncCompletionOption, $CancellationToken, $null).Result
+
+    }
+    catch {
+        Invoke-Exception($_.Exception)
+    }
+}
+
 # Issue 157 Implement Set-OpenStackDatabaseInstanceSize
 function Set-OpenStackDatabaseInstanceSize {
     Param(
@@ -380,16 +426,9 @@ function Set-OpenStackDatabaseInstanceSize {
 
 
         $Flavor = New-Object([net.openstack.Providers.Rackspace.Objects.Databases.FlavorRef]) $FlavorRef
-        Write-Host "Flavor Ref"
-
         $dbiid = New-Object([net.openstack.Providers.Rackspace.Objects.Databases.DatabaseInstanceId]) $InstanceId
-        Write-Host "Instance ID"
-
         $CancellationToken = New-Object ([System.Threading.CancellationToken]::None)
-        Write-Host "Cancellation Token"
-
         $AsyncCompletionOption = New-Object ([net.openstack.Core.AsyncCompletionOption])
-        Write-Host "Completion Option"
         
         $ComputeDatabasesProvider.ResizeDatabaseInstanceAsync($dbiid, $Flavor, $AsyncCompletionOption, $CancellationToken, $null).Result
 
